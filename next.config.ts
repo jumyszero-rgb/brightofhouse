d55433f29be04b936b0cc4afecea316a1cc3d258
Junji Yokoyama <jumys.zero@gmail.com>
2026-02-19 23:29:10 +0900

    Add Docker config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Dockerコンテナデプロイ用に、必要なファイルだけを出力する設定
+  output: "standalone",
 };
 
 export default nextConfig;
