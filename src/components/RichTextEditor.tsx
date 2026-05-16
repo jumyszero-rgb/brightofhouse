a62b839a55d21ec0b139c1a5d05e794844daece1
Junji Yokoyama <jumys.zero@gmail.com>
2026-05-17 06:20:59 +0900

    202605270620

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -12,10 +12,10 @@ import { TextStyle } from "@tiptap/extension-text-style";
 import Highlight from "@tiptap/extension-highlight";
 import TextAlign from "@tiptap/extension-text-align";
 import Placeholder from "@tiptap/extension-placeholder";
-import Table from "@tiptap/extension-table";
-import TableRow from "@tiptap/extension-table-row";
-import TableCell from "@tiptap/extension-table-cell";
-import TableHeader from "@tiptap/extension-table-header";
+import { Table } from "@tiptap/extension-table";
+import { TableRow } from "@tiptap/extension-table-row";
+import { TableCell } from "@tiptap/extension-table-cell";
+import { TableHeader } from "@tiptap/extension-table-header";
 import { Node, mergeAttributes, Extension } from "@tiptap/core";
 
 // --- FontSize 拡張（自前実装） ---
